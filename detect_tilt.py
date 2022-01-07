10da2fe7ddd4ada3bf66a3204a29953d1c1d9e5b
Hao-Ping <haopingchen1999@gmail.com>
2022-01-07 08:04:04 +0000

    2nd

--- detect_tilt.py
+++ detect_tilt.py
@@ -4,11 +4,11 @@ from bluepy.btle import Scanner, DefaultDelegate
 class ScanDelegate(DefaultDelegate):
 	def __init__(self):
 		DefaultDelegate.__init__(self)
-	def handleDiscovery(self, dev, isNewDev, isNewData):
-		if isNewDev:
-			print "Discovered device", dev.addr
-		elif isNewData:
-			print "Received new data from", dev.addr
+	#def handleDiscovery(self, dev, isNewDev, isNewData):
+		#if isNewDev:
+			#print "Discovered device", dev.addr
+		#elif isNewData:
+			#print "Received new data from", dev.addr
 #scanner = Scanner().withDelegate(ScanDelegate())
 #devices = scanner.scan(2.0)
 n=0
@@ -18,19 +18,21 @@ crash = False
 
 while(True):
     scanner = Scanner().withDelegate(ScanDelegate())
+    print 'Scanning for %d seconds.......' % scan_time
     devices = scanner.scan(scan_time)
-    print('Scanning for ', scan_time, 'seconds.......')
 
     for dev in devices:
-        print '%d: Device #%s (%s), RSSI=%d dB' % (n, dev.addr, dev.addrType, dev.rssi)
+        #print '%d: Device %s (%s), RSSI=%d dB' % (n, dev.addr, dev.addrType, dev.rssi)
         candidate = []
+
         for(adtype, desc, value) in dev.getScanData():
             candidate.append(value)
 
         if candidate[-1][:4] != 'Bike':
             candidate = []
         elif candidate[-1][:4] == 'Bike':
+            
             if candidate[-2][-2:] == '0f':
-                print(candidate[-1][:4], 'crash')
+                print '%s CRASH' % candidate[-1][:4] 
                 #clear crash state
                 candidate = []
