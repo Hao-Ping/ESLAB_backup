4499c0d25737a67c40375f1053966ef1d7ad5e4f
Hao-Ping <49761419+Hao-Ping@users.noreply.github.com>
2022-01-21 13:43:10 +0800

    fix 9999

--- detect_crash_advertise.py
+++ detect_crash_advertise.py
@@ -148,7 +148,7 @@ class CrashAdvertisement(Advertisement):
         self.add_service_uuid('180D')
         self.add_service_uuid('180F')
         self.add_manufacturer_data(0xffff, [0x00, 0x01, 0x02, 0x03])
-        self.add_service_data('', [0x00, 0x01, 0x02, 0x03, 0x03])
+        self.add_service_data('9999', [0x00, 0x01, 0x02, 0x03, 0x03])
         self.add_local_name('Road_Server')
         self.include_tx_power = True
         self.add_data(0x26, [0x01, 0x01, 0x00])
